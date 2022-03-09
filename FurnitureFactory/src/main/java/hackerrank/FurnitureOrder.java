3d52105cf18885ac0b7a23fc888597cb45f316d1
unknown <ashishpatil_312@yahoo.com>
2022-03-09 09:00:11 +0000

    Implementation

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -1,45 +1,58 @@
 package hackerrank;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
-    /**
-     * TODO: Create a map of Furniture items to order quantities
-     */
+	private Map<String, Float> furniture;
 
-    /**
-     * Initialize a new mapping of Furniture types to order quantities.
-     */
-    FurnitureOrder() {
-        // TODO: Complete the constructor
-    }
+	/**
+	 * Initialize a new mapping of Furniture types to order quantities.
+	 */
+	FurnitureOrder() {
+		this.furniture = new HashMap<String, Float>();
+	}
 
-    public void addToOrder(final Furniture type, final int furnitureCount) {
-        // TODO: Complete the method
-    }
+	public void addToOrder(final Furniture type, final int furnitureCount) {
+		this.furniture.put(type.label(), type.cost() * furnitureCount);
+	}
 
-    public HashMap<Furniture, Integer> getOrderedFurniture() {
-        // TODO: Complete the method
-        return null;
-    }
+	public HashMap<Furniture, Integer> getOrderedFurniture() {
+		HashMap<Furniture, Integer> totalOrder = new HashMap<Furniture, Integer>();
+		for (Entry<String, Float> e : this.furniture.entrySet()) {
 
-    public float getTotalOrderCost() {
-        // TODO: Complete the method
-        return -1.0f;
-    }
+			totalOrder.put(Furniture.valueOf(e.getKey().toUpperCase()),
+					(int) (e.getValue() / (Furniture.valueOf(e.getKey().toUpperCase()).cost())));
+		}
+		return totalOrder;
+	}
 
-    public int getTypeCount(Furniture type) {
-        // TODO: Complete the method
-        return -1;
-    }
+	public float getTotalOrderCost() {
+		float total = 0.0f;
+		for (float f : this.furniture.values()) {
+			total = total + f;
+		}
+		return total;
+	}
 
-    public float getTypeCost(Furniture type) {
-        // TODO: Complete the method
-        return -1.0f;
-    }
+	public int getTypeCount(Furniture type) {
+		if (this.furniture.get(type.label()) != null)
+			return (int) (this.furniture.get(type.label()) / type.cost());
+		return 0;
+	}
 
-    public int getTotalOrderQuantity() {
-        // TODO: Complete the method
-        return -1;
-    }
+	public float getTypeCost(Furniture type) {
+		if (this.furniture.get(type.label()) != null)
+			return this.furniture.get(type.label());
+		return 0.0f;
+	}
+
+	public int getTotalOrderQuantity() {
+		int totalOrder = 0;
+		for (Entry<String, Float> e : this.furniture.entrySet()) {
+			totalOrder = (int) (totalOrder + e.getValue() / (Furniture.valueOf(e.getKey().toUpperCase()).cost()));
+		}
+		return totalOrder;
+	}
 }
